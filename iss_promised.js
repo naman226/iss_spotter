fad24e3dfc2021174558fd7a81a1c18901fe9411
Naman Dhalla <namandhalla2261994@gmail.com>
2022-06-10 04:10:55 +0000

    Refactor with promises

---
+++ iss_promised.js
@@ -0,0 +1,26 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request("httprmatjsonp&");
+};
+
+const fetchCoordsByIP = function(body) {
+  return request(`https://api.ipbase.com/v1/json/${body}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+module.exports = { nextISSTimesForMyLocation };
